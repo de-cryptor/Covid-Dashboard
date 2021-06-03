e3852c820072b02284fcbb3e831ca1578bb7ba5c
de-cryptor <bim2014001@iiita.ac.in>
2021-06-03 22:53:53 +0530

    Handled empty query results

--- app/views.py
+++ app/views.py
@@ -30,25 +30,25 @@ class Oxygen(View):
     def get(self,request):
         query = request.GET.get('query', 'covid oxygen');
         data = fetch_tweets(query)
-        return render(request, 'resources.html', {'type' : 'oxygen', 'page_title' : 'Oxygen Availability Leads on Twitter' , 'data': data['data']})
+        return render(request, 'resources.html', {'type' : 'oxygen', 'page_title' : 'Oxygen Availability Leads on Twitter' , 'data': data.get('data', [])})
         
 class Plasma(TemplateView):
     def get(self,request):
         query = request.GET.get('query', 'covid blood plasma');
         data = fetch_tweets(query)
-        return render(request, 'resources.html', {'type' : 'plasma', 'page_title' : 'Plasma Availability Leads on Twitter' , 'data': data['data']})
+        return render(request, 'resources.html', {'type' : 'plasma', 'page_title' : 'Plasma Availability Leads on Twitter' , 'data': data.get('data', [])})
         
 class Beds(TemplateView):
     def get(self,request):
         query = request.GET.get('query', 'covid beds');
         data = fetch_tweets(query)
-        return render(request, 'resources.html', {'type' : 'beds', 'page_title' : 'Beds Availability Leads on Twitter' , 'data': data['data']})
+        return render(request, 'resources.html', {'type' : 'beds', 'page_title' : 'Beds Availability Leads on Twitter' , 'data': data.get('data', [])})
         
 class Medicines(TemplateView):
     def get(self,request):
         query = request.GET.get('query', 'covid medicines');
         data = fetch_tweets(query)
-        return render(request, 'resources.html', {'type' : 'medicines', 'page_title' : 'Medicines Availability Leads on Twitter' , 'data': data['data']})
+        return render(request, 'resources.html', {'type' : 'medicines', 'page_title' : 'Medicines Availability Leads on Twitter' , 'data': data.get('data', [])})
         
 class Donation(TemplateView):
     def get(self,request):
